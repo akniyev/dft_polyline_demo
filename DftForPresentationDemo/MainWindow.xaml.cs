7cd037e4ee08f5f5f0d110b2f7abd628e48022fa
Hasan A <hasan.akniyev@gmail.com>
2016-03-10 17:15:33 +0300

    Added project files

---
+++ DftForPresentationDemo/MainWindow.xaml.cs
@@ -0,0 +1,339 @@
+﻿using DiscreteFunctionsPlots;
+using GraphBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace DftForPresentationDemo
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        int m;
+        double[] vertexes;
+        double[] vertexes_y;
+        int _N = 16;
+        int N
+        {
+            get
+            {
+                return _N;
+            }
+            set
+            {
+                _N = value;
+                lbl_N.Content= _N.ToString();
+                if (n > _N) n = _N;
+                txt_n.Text = n.ToString();
+            }
+        }
+
+        int n = 8;
+
+        GraphBuilder2DForm gb = new GraphBuilder2DForm(2);
+        DiscreteFunctions.DiscreteFunction2D df;
+        Plot2D plot = new Plot2D("DF");
+        Plot2D dftPlot = new Plot2D("Fourier");
+        Plot2D diffPlot = new Plot2D("Diff");
+        Plot2D estPlot = new Plot2D("Estimate");
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            lbl_N.Content = N.ToString();
+            gb.Show();
+            gb.GraphBuilder.DrawPlot(plot);
+            gb.GetGraphBuilder(1).DrawPlot(estPlot);
+            gb.GetGraphBuilder(1).DrawPlot(diffPlot);
+            gb.GraphBuilder.DrawPlot(dftPlot);
+        }
+
+        private void button_Click(object sender, RoutedEventArgs e)
+        {
+            var tr = new Transformers.FastFourierTransformer();
+
+            var forward = tr.Transform(df.Y.Take(df.Y.Length - 1).ToArray());
+            var forward_abs = forward.Select(x => Math.Sqrt(x.x * x.x + x.y * x.y)).ToArray();
+
+
+            var skipCount = forward.Length / 2 - n;
+
+            for (int i = 0; i < skipCount; i++)
+            {
+                forward[forward.Length / 2 + i] = 0;
+                forward[forward.Length / 2 - i] = 0;
+            }
+
+            var backward = tr.InvTransform(forward);
+            var backward_list = backward.ToList();
+            backward_list.Add(backward[0]);
+            backward = backward_list.ToArray();
+
+
+            var df_dft = ((forward.Length) / 2.0) * new DiscreteFunctions.DiscreteFunctionComplex2D(backward).Re();
+
+            df_dft.X = df.X;
+
+            dftPlot.DiscreteFunction = df_dft;
+
+            dftPlot.Refresh();
+
+            diffPlot.DiscreteFunction = (plot.DiscreteFunction - dftPlot.DiscreteFunction).Abs();
+
+            diffPlot.Refresh();
+
+            //Drawing estimate for S_n(f,x)
+            //double[] ksi = new double[m + 1];
+            //for (int i = 0; i < m + 1; i++)
+            //{
+            //    ksi[i] = vertexes[i] - Math.PI;
+            //}
+
+            //double[] alpha = new double[m];
+            //double[] beta = new double[m];
+
+            //double[] f = new double[m + 1];
+
+            //for (int i = 0; i < m + 1; i++)
+            //{
+            //    f[i] = vertexes_y[i];
+            //}
+
+            //for (int i = 0; i < m; i++)
+            //{
+            //    alpha[i] = (f[i + 1] - f[i]) / (ksi[i + 1] - ksi[i]);
+            //    beta[i] = f[i] - (ksi[i] * (f[i + 1] - f[i]) / (ksi[i + 1] - ksi[i]));
+            //}
+
+            //int dN = 10 * N;
+
+            //double[] est = new double[dN];
+            //double[] _x = new double[dN];
+
+            //for (int i = 0; i < dN; i++)
+            //{
+            //    _x[i] = -Math.PI + 2 * Math.PI * i / (dN - 1);
+            //}
+
+            //for (int i = 0; i < dN; i++)
+            //{
+            //    double est_i = 0;
+
+            //    for (int k = n + 1; k < 1000; k++)
+            //    {
+            //        double inner_sum = 0;
+            //        for (int j = 0; j < m; j++)
+            //        {
+            //            //inner_sum += alpha[j] * Math.Sin(k * ((ksi[j + 1] + ksi[j]) / 2.0 - _x[i])) * Math.Sin(k * ((ksi[j + 1] + ksi[j]) / 2.0));
+            //            inner_sum += alpha[j] * (Math.Cos((ksi[j + 1] - _x[i]) * k) - Math.Cos((ksi[j] - _x[i]) * k));
+            //        }
+            //        inner_sum *= 1.0 / (k * k);
+
+            //        est_i += inner_sum;
+            //    }
+            //    est_i *= 1.0 / Math.PI;
+            //    est[i] = est_i;
+            //}
+
+            //double[] _x_shifted = new double[dN];
+
+            //for (int i = 0; i < dN; i++)
+            //{
+            //    _x_shifted[i] = _x[i] + Math.PI;
+            //}
+            //var dfEst = new DiscreteFunctions.DiscreteFunction2D(_x_shifted, est);
+
+            //estPlot.DiscreteFunction = dfEst.Abs();
+
+            //estPlot.Refresh();
+
+            //
+        }
+
+        private void btn_N_plus_Click(object sender, RoutedEventArgs e)
+        {
+            if (N < 64000) N *= 2;
+        }
+
+        private void btn_N_minus_Click(object sender, RoutedEventArgs e)
+        {
+            if (N > 4) N /= 2;
+        }
+
+        private void textBox_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            
+        }
+
+        private void txt_n_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                var b = "";
+
+                for (int i = 0; i < txt_n.Text.Length; i++)
+                {
+                    if (txt_n.Text[i] <= '9' && txt_n.Text[i] >= '0')
+                    {
+                        b += txt_n.Text[i];
+                    }
+                }
+
+                txt_n.Text = b;
+
+                var temp_n = b != "" ? int.Parse(b) : 0;
+
+                if (temp_n < 2) temp_n = 2;
+
+                if (temp_n > N) temp_n = N;
+
+                n = temp_n;
+            }
+        }
+
+        private void txt_n_LostFocus(object sender, RoutedEventArgs e)
+        {
+            var b = "";
+
+            for (int i = 0; i < txt_n.Text.Length; i++)
+            {
+                if (txt_n.Text[i] <= '9' && txt_n.Text[i] >= '0')
+                {
+                    b += txt_n.Text[i];
+                }
+            }
+
+            txt_n.Text = b;
+
+            var temp_n = b != "" ? int.Parse(b) : 0;
+
+            if (temp_n < 2) temp_n = 2;
+
+            if (temp_n > N) temp_n = N;
+
+            n = temp_n;
+        }
+
+        double[] GenerateGrid(int count)
+        {
+            var result = new double[count + 1];
+
+            for (int i = 0; i <= count; i++)
+            {
+                result[i] = 2 * Math.PI * i / N;
+            }
+
+            return result;
+        }
+
+        private void btn_draw_abs_Click(object sender, RoutedEventArgs e)
+        {
+            var x = GenerateGrid(N);
+            var y = new double[x.Length];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = Math.Abs(x[i] - Math.PI);
+            }
+
+            df = new DiscreteFunctions.DiscreteFunction2D(x, y);
+
+            plot.DiscreteFunction = df;
+
+            plot.Refresh();
+        }
+
+        private void btn_draw_sign_Click(object sender, RoutedEventArgs e)
+        {
+            var x = GenerateGrid(N);
+            var y = new double[x.Length];
+
+            for (int i = 0; i < x.Length; i++)
+            {
+                y[i] = Math.Sign(x[i] - Math.PI);
+            }
+
+            df = new DiscreteFunctions.DiscreteFunction2D(x, y);
+
+            plot.DiscreteFunction = df;
+
+            plot.Refresh();
+        }
+
+        private void btn_draw_chain_Click(object sender, RoutedEventArgs e)
+        {
+            m = int.Parse(txt_m.Text);
+            var min = int.Parse(txt_min.Text);
+            var max = int.Parse(txt_max.Text);
+
+            var x = GenerateGrid(N);
+            var y = new double[x.Length];
+
+            vertexes = new double[m + 1];
+            var delta = Math.PI * 2.0 / m;
+            var r = new Random();
+            for (int i = 1; i < m; i++)
+            {
+                vertexes[i] = delta / 2 + (i - 1) * delta + r.NextDouble() * delta;
+            }
+
+            vertexes[0] = 0;
+            vertexes[m] = 2 * Math.PI;
+
+            vertexes_y = new double[m + 1];
+
+            for (int i = 0; i < m; i++)
+            {
+                vertexes_y[i] = r.NextDouble() * (max - min) + min;
+            }
+
+            vertexes_y[m] = vertexes_y[0];
+
+
+
+            for (int i = 0; i < x.Length - 1; i++)
+            {
+                int vertexId = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    if (vertexes[j] <= x[i] && vertexes[j + 1] >= x[i])
+                    {
+                        vertexId = j;
+                        break;
+                    }
+                }
+
+                double a = vertexes[vertexId];
+                double b = vertexes[vertexId + 1];
+
+                double c = x[i];
+
+                double f_a = vertexes_y[vertexId];
+                double f_b = vertexes_y[vertexId + 1];
+
+                y[i] = f_b * (c - a) / (b - a) + f_a * (b - c) / (b - a);
+            }
+
+            y[y.Length - 1] = y[0];
+
+            df = new DiscreteFunctions.DiscreteFunction2D(x, y);
+
+            plot.DiscreteFunction = df;
+
+            plot.Refresh();
+        }
+    }
+}
---
+++ Polyline/MainWindow.xaml.cs
@@ -0,0 +1,412 @@
+﻿using DiscreteFunctions;
+using DiscreteFunctionsPlots;
+using GraphBuilders;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+//using static.System.Math;
+
+namespace Polyline
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        GraphBuilder2DForm gb = new GraphBuilder2DForm(2);
+        DiscreteFunction2D df;
+        Plot2D plot_polyline = new Plot2D("Polyline");
+        Plot2D plot_discrete_polyline = new Plot2D("Discrete Polyline");
+        Plot2D plot_check_alpha_beta = new Plot2D("Check alpha and beta");
+        Plot2D plot_fourier = new Plot2D("Fourier");
+        Plot2D plot_dft = new Plot2D("DFT");
+        Plot2D plot_fourier_diff = new Plot2D("Fourier diff");
+        Plot2D plot_calculated_fourier_diff = new Plot2D("Calculated Fourier Diff");
+        Plot2D plot_calculated_dft_diff = new Plot2D("Calculated DFT Diff");
+
+        Plot2D plot_uniform_estimate = new Plot2D("Uniform diff estimate");
+
+        //Polyline params
+        double[] x_polyline;
+        double[] y_polyline;
+
+        double[] x_discrete_polyline;
+        double[] y_discrete_polyline;
+
+        double[] alpha_polyline;
+        double[] beta_polyline;
+
+        int m;
+        double min_y;
+        double max_y;
+
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            gb.GraphBuilder.DrawPlot(plot_polyline);
+            gb.GraphBuilder.DrawPlot(plot_discrete_polyline);
+            gb.GraphBuilder.DrawPlot(plot_check_alpha_beta);
+            gb.GraphBuilder.DrawPlot(plot_fourier);
+            gb.GraphBuilder.DrawPlot(plot_dft);
+            gb.GetGraphBuilder(1).DrawPlot(plot_fourier_diff);
+            gb.GetGraphBuilder(1).DrawPlot(plot_calculated_fourier_diff);
+            gb.GetGraphBuilder(1).DrawPlot(plot_calculated_dft_diff);
+            gb.GetGraphBuilder(1).DrawPlot(plot_uniform_estimate);
+            gb.Show();
+        }
+
+        double a0_2()
+        {
+            double[] ksi = x_polyline;
+
+            double a0_2 = 0;
+            for (int j = 0; j < m; j++)
+            {
+                a0_2 += alpha_polyline[j] / 2.0 * (ksi[j + 1] * ksi[j + 1] - ksi[j] * ksi[j]) + beta_polyline[j] * (ksi[j + 1] - ksi[j]);
+            }
+            a0_2 *= 1.0 / (Math.PI * 2);
+
+            return a0_2;
+        }
+
+        double a_(int k)
+        {
+            double[] ksi = x_polyline;
+            double a_k = 0;
+            for (int j = 0; j < m; j++)
+            {
+                a_k += alpha_polyline[j] * (Math.Cos(k * ksi[j + 1]) - Math.Cos(k * ksi[j]));
+            }
+
+            a_k *= 1.0 / (Math.PI * k * k);
+
+            return a_k;
+        }
+
+        double b_(int k)
+        {
+            double[] ksi = x_polyline;
+            double b_k = 0;
+            for (int j = 0; j < m; j++)
+            {
+               b_k += alpha_polyline[j] * (Math.Sin(k * ksi[j + 1]) - Math.Sin(k * ksi[j]));
+            }
+
+            b_k *= 1.0 / (Math.PI * k * k);
+
+            return b_k;
+        }
+
+        private void btn_generate_polyline_Click(object sender, RoutedEventArgs e)
+        {
+            m = int.Parse(txt_m.Text);
+            min_y = double.Parse(txt_min_y.Text);
+            max_y = double.Parse(txt_max_y.Text);
+
+            var r = new Random();
+
+            x_polyline = new double[m - 1].Select(x => r.NextDouble() * 2 * Math.PI - Math.PI).Union(new double[] { Math.PI, -Math.PI }).OrderBy(y => y).ToArray();
+            y_polyline = x_polyline.Select(x => r.NextDouble() * (max_y - min_y) + min_y).ToArray();
+            y_polyline[0] = y_polyline[m];
+
+            plot_polyline.DiscreteFunction = new DiscreteFunction2D(x_polyline, y_polyline);
+            plot_polyline.Refresh();
+
+            //Computing alpha and beta
+            alpha_polyline = new double[m];
+            beta_polyline = new double[m];
+
+            for (int i = 0; i < m; i++)
+            {
+                double x1, x2, y1, y2;
+                x1 = x_polyline[i];
+                x2 = x_polyline[i + 1];
+                y1 = y_polyline[i];
+                y2 = y_polyline[i + 1];
+
+                alpha_polyline[i] = (y2 - y1) / (x2 - x1);
+                beta_polyline[i] = y1 - alpha_polyline[i] * x1;
+            }
+
+            //Generating discrete polyline
+            int N = int.Parse(txt_DFT_N.Text);
+            int currentSegment = 0;
+            x_discrete_polyline = new double[N];
+            y_discrete_polyline = new double[N];
+
+            for (int i = 0; i < N; i++)
+            {
+                double x = Math.PI * 2.0 * i / N - Math.PI;
+                x_discrete_polyline[i] = x;
+                while (x > x_polyline[currentSegment + 1])
+                    currentSegment++;
+                y_discrete_polyline[i] = alpha_polyline[currentSegment] * x + beta_polyline[currentSegment];
+            }
+
+            plot_discrete_polyline.DiscreteFunction = new DiscreteFunction2D(x_discrete_polyline, y_discrete_polyline);
+            plot_discrete_polyline.Refresh();
+
+            ////Cheking alpha and beta
+            //int currentSegment = 0;
+            //double[] check_x = new double[m * 1000 + 1];
+            //double[] check_y = new double[m * 1000 + 1];
+
+            //for (int i = 0; i <= m * 1000; i++)
+            //{
+            //    double x = Math.PI * 2.0 * i / (m * 1000.0) - Math.PI;
+            //    check_x[i] = x;
+            //    while (x > x_polyline[currentSegment+1])
+            //        currentSegment++;
+            //    check_y[i] = alpha_polyline[currentSegment] * x + beta_polyline[currentSegment];
+            //}
+
+            //plot_check_alpha_beta.DiscreteFunction = new DiscreteFunction2D(check_x, check_y);
+            //plot_check_alpha_beta.Refresh();
+        }
+
+        private void btn_generate_S_n_Click(object sender, RoutedEventArgs e)
+        {
+            //Checking Fourier series
+            //Вычисляем частичную сумму ряда Фурье
+            int Dens = int.Parse(txt_Dens.Text);
+            int n = int.Parse(txt_n.Text);
+
+            double[] x_fourier = new double[Dens + 1];
+            double[] y_fourier = new double[Dens + 1];
+            double[] ksi = x_polyline;
+
+            for (int i = 0; i <= Dens; i++)
+            {
+
+                double x = 2.0 * Math.PI / Dens * i - Math.PI;
+                x_fourier[i] = x;
+                double y = 0;
+
+                //Здесь начинается новый способ вычисления (выведенный по формулам)
+                //Новый способ тоже работает, как и старый
+                double a0 = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    a0 += alpha_polyline[j] / 2.0 * (ksi[j+1] * ksi[j+1] - ksi[j] * ksi[j]) + beta_polyline[j] * (ksi[j+1] - ksi[j]);
+                    
+                }
+                a0 *= 1.0 / (2.0 * Math.PI);
+                y += a0;
+
+                double sum_k = 0;
+                for (int k = 1; k <= n; k++)
+                {
+                    double sum_j = 0;
+                    for (int j = 0; j < m; j++)
+                    {
+                        sum_j += alpha_polyline[j] * (Math.Cos(k * x) * (Math.Cos(k * ksi[j + 1]) - Math.Cos(k * ksi[j])) + Math.Sin(k * x) * (Math.Sin(k * ksi[j + 1]) - Math.Sin(k * ksi[j])));
+                    }
+                    sum_j *= 1.0 / (k * k);
+
+                    sum_k += sum_j;
+                }
+                sum_k *= 1.0 / Math.PI;
+
+                y += sum_k;
+
+                //Это старый способ вычисления, вместо него попробуем новый
+                //y = a0_2();
+                //for (int k = 1; k <= n; k++)
+                //{
+                //    y += a_(k) * Math.Cos(k * x) + b_(k) * Math.Sin(k * x);
+                //}
+                y_fourier[i] = y;
+
+            }
+            plot_fourier.DiscreteFunction = new DiscreteFunction2D(x_fourier, y_fourier);
+            plot_fourier.Refresh();
+
+            //Вычисляем дискретную сумму ряда Фурье на густой сетке
+            int N = y_discrete_polyline.Length;
+
+            var tr = new Transformers.FastFourierTransformer();
+            var forward = tr.Transform(y_discrete_polyline);
+
+            var y_inverse = tr.InvTransform(forward).Select(x => Math.Sqrt(x.x * x.x + x.y * x.y)).ToArray();
+
+            double[] x_inverse = new double[y_discrete_polyline.Length];
+
+            var x_inverse_added = ;
+
+            for (int i = 0; i < N; i++)
+            {
+                x_inverse[i] = 2.0 * Math.PI * i / N;
+            }
+
+            plot_dft.DiscreteFunction = new DiscreteFunction2D(x_inverse, y_inverse);
+
+
+
+            //Diff f - fourier
+            //Берем разность функции и вычисленного выше ряда Фурье
+            double[] diff = new double[Dens + 1];
+            int currentSegment = 0;
+
+            for (int i = 0; i <= Dens; i++)
+            {
+                double x = Math.PI * 2.0 * i / Dens - Math.PI;
+                while (x > x_polyline[currentSegment + 1])
+                    currentSegment++;
+                double y = alpha_polyline[currentSegment] * x + beta_polyline[currentSegment];
+                diff[i] = y - y_fourier[i];
+            }
+
+            plot_fourier_diff.DiscreteFunction = new DiscreteFunction2D(x_fourier, diff);
+            plot_fourier_diff.Refresh();
+
+            //Calculated Fourier Diff
+            //Вычисляем самостоятельно остаток ряда Фурье (естественно, не до бесконечности, а до 1000 или около того)
+
+            int j_skipped = int.Parse(txt_j_to_skip.Text);
+
+            double[] calculated_diff_x = new double[Dens + 1];
+            double[] calculated_diff_y = new double[Dens + 1];
+            //for (int i = 0; i <= N; i++)
+            //{
+            //    double x = Math.PI * 2.0 * i / N - Math.PI;
+            //    calculated_diff_x[i] = x;
+            //    double sum_k = 0;
+            //    for (int k = n + 1; k < 1000; k++)
+            //    {
+            //        double sum_j = 0;
+            //        for (int j = 1; j <= m; j++)
+            //        {
+            //            int _j = j;
+            //            int _j_1 = j - 1;
+
+            //            if (_j == m) _j = 0;
+
+            //            if (j_skipped != -1 && j != j_skipped) continue;
+            //            sum_j += (alpha_polyline[_j_1] - alpha_polyline[_j]) * Math.Cos(k * (ksi[_j] - x));
+            //        }
+            //        //sum_j += (alpha_polyline[m - 1] - alpha_polyline[0]) * Math.Cos(k * (x + Math.PI));
+            //        sum_j *= 1.0 / (k * k);
+
+            //        sum_k += sum_j;
+            //    }
+            //    sum_k *= 1.0 / Math.PI;
+            //    calculated_diff_y[i] = sum_k;
+            //}
+
+            for (int i = 0; i <= Dens; i++)
+            {
+                double x = Math.PI * 2.0 * i / Dens - Math.PI;
+                calculated_diff_x[i] = x;
+                double sum_j = 0;
+                for (int j = 1; j <= m; j++)
+                {
+                    if (j_skipped != -1 && j != j_skipped) continue;
+                    double sum_k = 0;
+                    for (int k = n + 1; k <= 1000 + n + 1; k++)
+                    {
+                        sum_k += Math.Cos(k * (ksi[j] - x)) / (k * k);
+                    }
+                    //sum_j += (alpha_polyline[m - 1] - alpha_polyline[0]) * Math.Cos(k * (x + Math.PI));
+                    double alpha_j = alpha_polyline[j % m];
+                    double alpha_j_1 = alpha_polyline[(j - 1) % m];
+                    sum_k *= alpha_j_1 - alpha_j;
+
+                    sum_j += sum_k;
+                }
+                sum_j *= 1.0 / Math.PI;
+                calculated_diff_y[i] = sum_j;
+            }
+
+            plot_calculated_fourier_diff.DiscreteFunction = new DiscreteFunction2D(calculated_diff_x, calculated_diff_y);
+            plot_calculated_fourier_diff.Refresh();
+
+            //Здесь вычисляем вычисленный остаток для дискретных рядов Фурье
+            //int N = int.Parse(txt_DFT_N.Text);
+            //int dft_n = int.Parse(txt_DFT_n.Text);
+
+            //double[] calculated_dft_diff_x = new double[Dens + 1];
+            //double[] calculated_dft_diff_y = new double[Dens + 1];
+
+            //for (int i = 0; i <= Dens; i++)
+            //{
+            //    double x = Math.PI * 2.0 * i / Dens - Math.PI;
+            //    calculated_dft_diff_x[i] = x;
+
+            //    double R1 = 0;
+            //    for (int mu = 1; mu < 1000; mu++)
+            //    {
+            //        double sum_j = 0;
+            //        for (int j = 1; j <= m; j++)
+            //        {
+            //            int _j = j % m;
+            //            sum_j += (alpha_polyline[j - 1] - alpha_polyline[_j]) * Math.Cos(mu * Dens * ksi[j]);
+            //        }
+            //        sum_j *= 1.0 / (mu * mu);
+
+            //        R1 += sum_j;
+            //    }
+            //    R1 *= 1.0 / (Math.PI * N * N);
+
+            //    double R2 = 0;
+
+            //    for (int mu = 0; mu < 1000; mu++)
+            //    {
+            //        double sum_k = 0;
+            //        for (int k = 1; k <= dft_n; k++)
+            //        {
+            //            double sum_j = 0;
+            //            for (int j = 1; j<= m; j++)
+            //            {
+            //                sum_j += (alpha_polyline[j - 1] - alpha_polyline[j % m]) * ((Math.Cos(k * (x - ksi[j]) + mu * N * ksi[j])) / (Math.Pow(k - mu * N, 2) + (Math.Cos(k * (x - ksi[j]) - mu * N * ksi[j])) / (Math.Pow(k + mu * N, 2))));
+            //            }
+            //            sum_k += sum_j;
+            //        }
+            //        R2 += sum_k;
+            //    }
+            //    R2 *= 1.0 / Math.PI;
+
+            //    double R = R1 + R2;
+
+            //    calculated_dft_diff_y[i] = R;
+            //}
+
+            //plot_calculated_dft_diff.DiscreteFunction = new DiscreteFunction2D(calculated_dft_diff_x, calculated_dft_diff_y);
+            //plot_calculated_dft_diff.Refresh();
+
+            //Calculating uniform estimate
+            double[] calculated_diff_esimate_uniform_x = new double[2];
+            double[] calculated_diff_estimate_uniform_y = new double[2];
+
+            calculated_diff_esimate_uniform_x[0] = -Math.PI;
+            calculated_diff_esimate_uniform_x[1] = Math.PI;
+
+            double est = 1.0 / Math.PI / n;
+            double est_sum_j = 0;
+            for (int j = 1; j < m; j++)
+            {
+                est_sum_j += Math.Abs(alpha_polyline[j-1] - alpha_polyline[j]);
+            }
+            est_sum_j += Math.Abs(alpha_polyline[m-1] - alpha_polyline[0]);
+
+            est *= est_sum_j;
+
+            calculated_diff_estimate_uniform_y[0] = est;
+            calculated_diff_estimate_uniform_y[1] = est;
+
+            plot_uniform_estimate.DiscreteFunction = new DiscreteFunction2D(calculated_diff_esimate_uniform_x, calculated_diff_estimate_uniform_y);
+            plot_uniform_estimate.Refresh();
+        }
+    }
+}
